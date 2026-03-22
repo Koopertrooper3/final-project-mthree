d91ff15f6fe47ed332e1a0e82fe04c2b4a5777c4
Koopertrooper3 <cyu886116@gmail.com>
2026-03-22 00:30:39 -0400

    Implemented create list page frontend

--- frontend/src/app/create-list-page/create-list-page.component.ts
+++ frontend/src/app/create-list-page/create-list-page.component.ts
@@ -1,15 +1,85 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
-import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
+interface ingredient{
+  name: String
+  tags: String[]
+}
 
 @Component({
   selector: 'app-create-list-page',
   imports: [
     NavbarComponent,
+    ReactiveFormsModule,
   ],
   templateUrl: './create-list-page.component.html',
   styleUrl: './create-list-page.component.css'
 })
+
 export class CreateListPageComponent {
 
+
+  currentIngredientTags : string[] = ["Vegetable","Crame","Protein-Rich","afjlgbnsjkldngjkadsn gkasdbn","test","test","test","test"]
+  groceryList : ingredient[] = [
+    {
+      name: "Potato",
+      tags: ["Carbohydrate", "Vegetable"]
+    },
+    {
+      name: "Potato",
+      tags: ["Carbohydrate", "Vegetable","Acorn","Protein","Fatty", "Crunchy","Large","Heavy"]
+    }
+  ]
+  ingredientForm: FormGroup = new FormGroup({
+    ingredientName: new FormControl("",
+      [
+        Validators.required,
+      ]
+    ),
+    ingredientTag: new FormControl("",)
+  });
+
+  removeTag(index : number){
+    this.currentIngredientTags.splice(index,1)
+  }
+
+  removeTagFromIngredient(ingredientIndex :number, tagIndex :number){
+    this.groceryList[ingredientIndex].tags.splice(tagIndex,1);
+  }
+
+  get ingredientName(){
+    return this.ingredientForm.get('ingredientName');
+  }
+
+  get ingredientTag(){
+    return this.ingredientForm.get('ingredientTag') as FormControl;
+  }
+  
+
+  addIngredient(){
+    if(this.ingredientName && this.ingredientName.value.length != 0){
+      this.groceryList.push(
+        {
+          name: this.ingredientName?.value,
+          tags: this.currentIngredientTags
+        }
+      )
+      this.currentIngredientTags = []
+      this.ingredientForm.reset(this.ingredientName)
+      this.ingredientForm.reset(this.ingredientTag)
+
+    }
+    
+  }
+  addTag(){
+    let newTag = this.ingredientTag.value
+    this.currentIngredientTags.push(newTag)
+    this.ingredientForm.reset(this.ingredientTag)
+  }
+
+  onSubmit(){
+    console.log("submit works")
+  }
 }
